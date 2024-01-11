e1a5494c8512ab894dd2d9efedf7904d64395d68
VenncenzoCabezas <sazebacvenn@gmail.com>
2024-01-11 15:07:59 +0800

    commit changes

--- app/src/main/java/com/example/colorsensor/CaptureImageActivity.kt
+++ app/src/main/java/com/example/colorsensor/CaptureImageActivity.kt
@@ -1,176 +1,60 @@
 package com.example.colorsensor
 
 import android.Manifest
-import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
-import android.graphics.drawable.Drawable
+import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CameraMetadata
 import android.hardware.camera2.CaptureRequest
-import android.hardware.camera2.params.MeteringRectangle
 import android.media.ImageReader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
-import android.util.SparseIntArray
+import android.util.Log
+import android.util.Size
 import android.view.Surface
 import android.view.TextureView
-import android.view.View
 import android.widget.ImageView
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import java.io.File
 
 class CaptureImageActivity : AppCompatActivity() {
 
+    private lateinit var handlerThread: HandlerThread
+    private lateinit var cameraManager: CameraManager
+    private lateinit var capturedImageBytes: ByteArray
+    private var cameraFacing = CameraCharacteristics.LENS_FACING_BACK
     lateinit var capReq: CaptureRequest.Builder
     lateinit var handler: Handler
-    lateinit var handlerThread: HandlerThread
-    lateinit var cameraManager: CameraManager
     lateinit var textureView: TextureView
     lateinit var cameraCaptureSession: CameraCaptureSession
     lateinit var cameraDevice: CameraDevice
     lateinit var imageReader: ImageReader
-    lateinit var capturedImageBytes: ByteArray
-
-    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
-            openCamera()
-        }
-
-        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {}
-
-        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
-            return false
-        }
-
-        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
-    }
+    private var isFlashOn: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_capture_image)
         getPermissions()
-
-        textureView = findViewById(R.id.CameraTextureView)
-        textureView.surfaceTextureListener = surfaceTextureListener
-
-        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        handlerThread = HandlerThread("videoThread")
-        handlerThread.start()
-        handler = Handler((handlerThread as HandlerThread).looper)
-
-        imageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1)
-        imageReader.setOnImageAvailableListener(object : ImageReader.OnImageAvailableListener {
-            override fun onImageAvailable(reader: ImageReader?) {
-                val image = reader?.acquireLatestImage()
-                val buffer = image!!.planes[0].buffer
-                capturedImageBytes = ByteArray(buffer.remaining())
-                buffer.get(capturedImageBytes)
-                image.close()
-
-                // Save the captured image to a temporary file
-                val imageFile = saveImageToTempFile(capturedImageBytes)
-
-                // Pass the file path to the next activity
-                val intent = Intent(this@CaptureImageActivity, ColorPickerActivity::class.java)
-                intent.putExtra("capturedImagePath", imageFile.absolutePath)
-                startActivity(intent)
-            }
-        }, handler)
-
-
-
-
-        findViewById<ImageView>(R.id.HistoryImageView).apply {
-            setOnClickListener {
-                val intent = Intent(this@CaptureImageActivity, HistoryActivity::class.java)
-                startActivity(intent)
-            }
-        }
-
-        findViewById<ImageView>(R.id.CaptureImageView).apply {
-            setOnClickListener {
-                val rotation = windowManager.defaultDisplay.rotation
-                val sensorOrientation = cameraManager.getCameraCharacteristics(cameraManager.cameraIdList[0])
-                    ?.get(CameraCharacteristics.SENSOR_ORIENTATION) ?: 0
-
-                val jpegOrientation = (sensorOrientation + orientations.get(rotation) + 270) % 360
-
-                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE).apply {
-                    addTarget(imageReader.surface)
-                    set(CaptureRequest.JPEG_ORIENTATION, jpegOrientation)
-
-                    // Adding autofocus mode control
-                    set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
-                    set(CaptureRequest.CONTROL_AF_MODE, CameraMetadata.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
-                }
-                cameraCaptureSession.capture(capReq.build(), null, null)
-            }
-        }
-    }
-    // Function to save the image to a temporary file
-    private fun saveImageToTempFile(data: ByteArray): File {
-        val tempDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val imageFile = File.createTempFile("captured_image", ".jpg", tempDir)
-        imageFile.writeBytes(data)
-        return imageFile
-    }
-    private val orientations = SparseIntArray().apply {
-        append(Surface.ROTATION_0, 90)
-        append(Surface.ROTATION_90, 0)
-        append(Surface.ROTATION_180, 270)
-        append(Surface.ROTATION_270, 180)
+        cameraFlash()
+        cameraFlip()
+        cameraPreview()
+        captureImage()
+        openHistoryActivity()
     }
 
-    @SuppressLint("MissingPermission")
-    fun openCamera(){
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-        cameraManager.openCamera(cameraManager.cameraIdList[0], object: CameraDevice.StateCallback(){
-            override fun onOpened(pO: CameraDevice) {
-                cameraDevice = pO
-                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                var surface = Surface(textureView.surfaceTexture)
-                capReq.addTarget(surface)
-
-                cameraDevice.createCaptureSession(listOf(surface, imageReader.surface), object :
-                    CameraCaptureSession.StateCallback() {
-                    override fun onConfigured(pO: CameraCaptureSession) {
-                        cameraCaptureSession = pO
-                        cameraCaptureSession.setRepeatingRequest(capReq.build(),null,null)
-                    }
-
-                    override fun onConfigureFailed(session: CameraCaptureSession) {
-                    }
-
-                }, handler)
-            }
-
-            override fun onDisconnected(camera: CameraDevice) {
-            }
-
-            override fun onError(camera: CameraDevice, error: Int) {
-            }
-        }, handler)
-    }
-
-    fun getPermissions() {
-        var permissionList = mutableListOf<String>()
+    private fun getPermissions() {
+        val permissionList = mutableListOf<String>()
         if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) permissionList.add(
             android.Manifest.permission.CAMERA
         )
@@ -194,6 +78,216 @@ class CaptureImageActivity : AppCompatActivity() {
         grantResults.forEach {
             if (it != PackageManager.PERMISSION_GRANTED) {
                 getPermissions()
+            }
+        }
+    }
+
+    private fun chooseOptimalSize(choices: Array<Size>, width: Int, height: Int): Size {
+        val targetRatio = height.toDouble() / width
+        var optimalSize = choices[0]
+
+        for (size in choices) {
+            if (size.width.toDouble() / size.height == targetRatio) {
+                optimalSize = size
+                break
+            }
+        }
+
+        return optimalSize
+    }
+
+    private fun closeCamera() {
+        if (::cameraCaptureSession.isInitialized) {
+            cameraCaptureSession.close()
+        }
+        if (::cameraDevice.isInitialized) {
+            cameraDevice.close()
+        }
+    }
+
+    private fun openCamera() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        val cameraId = getCameraId(cameraFacing)
+
+        val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+        val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+        val previewSize = chooseOptimalSize(
+            map?.getOutputSizes(SurfaceTexture::class.java) ?: emptyArray(),
+            textureView.width,
+            textureView.height
+        )
+
+        textureView.surfaceTexture?.setDefaultBufferSize(previewSize.width, previewSize.height)
+
+        cameraManager.openCamera(
+            cameraId,
+            object : CameraDevice.StateCallback() {
+                override fun onOpened(pO: CameraDevice) {
+                    cameraDevice = pO
+                    capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                    var surface = Surface(textureView.surfaceTexture)
+                    capReq.addTarget(surface)
+
+                    val surfaceList = listOf(surface, imageReader.surface)
+                    cameraDevice.createCaptureSession(surfaceList, object :
+                        CameraCaptureSession.StateCallback() {
+                        override fun onConfigured(session: CameraCaptureSession) {
+                            cameraCaptureSession = session
+                            cameraCaptureSession.setRepeatingRequest(capReq.build(), null, null)
+                        }
+
+                        override fun onConfigureFailed(session: CameraCaptureSession) {
+                            // handle configuration failure
+                        }
+                    }, handler)
+                }
+
+                override fun onDisconnected(camera: CameraDevice) {
+                }
+
+                override fun onError(camera: CameraDevice, error: Int) {
+                    Log.e(TAG, "Camera error: $error")
+                }
+            },
+            handler
+        )
+    }
+
+    private fun getCameraId(facing: Int): String {
+        val cameraIds = cameraManager.cameraIdList
+        for (cameraId in cameraIds) {
+            val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+            val cameraDirection = characteristics.get(CameraCharacteristics.LENS_FACING)
+            if (cameraDirection == facing) {
+                return cameraId
+            }
+        }
+        //default to the first camera if no match is found
+        return cameraIds[0]
+    }
+
+    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+            closeCamera()
+            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(
+            surface: SurfaceTexture,
+            width: Int,
+            height: Int
+        ) {
+        }
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+    }
+
+    private fun cameraPreview() {
+        textureView = findViewById(R.id.CameraTextureView)
+        textureView.surfaceTextureListener = surfaceTextureListener
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        findViewById<ImageView>(R.id.CaptureImageView).setOnClickListener {
+            capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE).apply {
+                addTarget(imageReader.surface)
+
+                //autofocus mode control
+                set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+                set(
+                    CaptureRequest.CONTROL_AF_MODE,
+                    CameraMetadata.CONTROL_AF_MODE_CONTINUOUS_PICTURE
+                )
+            }
+            cameraCaptureSession.capture(capReq.build(), null, null)
+        }
+    }
+
+    private fun cameraFlash() {
+        val flashImageView = findViewById<ImageView>(R.id.CameraFlashImageView)
+
+        // Set initial drawable based on the initial state of isFlashOn
+        flashImageView.setImageResource(if (isFlashOn) R.drawable.flash else R.drawable.flash_off)
+
+        flashImageView.setOnClickListener {
+            isFlashOn = !isFlashOn
+            flashImageView.setImageResource(if (isFlashOn) R.drawable.flash else R.drawable.flash_off)
+            try {
+                val cameraId = cameraManager.cameraIdList[0]
+                //check if the flashlight is supposed to be on or off before toggling
+                if (isFlashOn) {
+                    //set flash mode to ON
+                    capReq.set(CaptureRequest.FLASH_MODE, CameraMetadata.FLASH_MODE_TORCH)
+                } else {
+                    //set flash mode to OFF
+                    capReq.set(CaptureRequest.FLASH_MODE, CameraMetadata.FLASH_MODE_OFF)
+                }
+                cameraCaptureSession.setRepeatingRequest(capReq.build(), null, null)
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun cameraFlip() {
+        val flipCameraImageView = findViewById<ImageView>(R.id.FlipCameraImageView)
+
+        flipCameraImageView.setOnClickListener {
+            cameraFacing = if (cameraFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                CameraCharacteristics.LENS_FACING_FRONT
+            } else {
+                CameraCharacteristics.LENS_FACING_BACK
+            }
+            openCamera()
+        }
+    }
+
+    private fun captureImage() {
+        imageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1)
+        imageReader.setOnImageAvailableListener({ reader ->
+            val image = reader?.acquireLatestImage()
+            val buffer = image!!.planes[0].buffer
+            capturedImageBytes = ByteArray(buffer.remaining())
+            buffer.get(capturedImageBytes)
+            image.close()
+
+            //save the captured image to a temporary file
+            val imageFile = saveImageToTempFile(capturedImageBytes)
+
+            //pass the file path to the next activity
+            val intent = Intent(this@CaptureImageActivity, ColorPickerActivity::class.java)
+
+            intent.putExtra("capturedImagePath", imageFile.absolutePath)
+            startActivity(intent)
+        }, handler)
+    }
+
+    private fun saveImageToTempFile(data: ByteArray): File {
+        val tempDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFile = File.createTempFile("captured_image", ".jpg", tempDir)
+        imageFile.writeBytes(data)
+        return imageFile
+    }
+
+    private fun openHistoryActivity() {
+        findViewById<ImageView>(R.id.HistoryImageView).apply {
+            setOnClickListener {
+                val intent = Intent(this@CaptureImageActivity, HistoryActivity::class.java)
+                startActivity(intent)
             }
         }
     }
